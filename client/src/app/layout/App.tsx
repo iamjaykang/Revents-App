008dcd438c9c8e8e0ba9120134c059c2b24426f3
Jay Kang <jaykang0395@gmail.com>
2022-12-05 04:30:39 +1300

    feat: re-render on path change for activity form

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,14 +1,20 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import Navbar from "./Navbar";
 import ActivityDashboard from "../../components/activities/dashboard/ActivityDashboard";
 import { observer } from "mobx-react-lite";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import HomePage from "../../components/home/HomePage";
 import ActivityForm from "../../components/activities/form/ActivityForm";
 import ActivityDetails from "../../components/activities/details/ActivityDetails";
 
 function App() {
+  let location = useLocation();
+
+  useEffect(() => {
+  }, [location])
+  
+
   return (
     <Fragment>
       <Navbar />
@@ -18,7 +24,7 @@ function App() {
           <Route path="/activities" element={<ActivityDashboard />} />
           <Route path="/activities/:id" element={<ActivityDetails />} />
           {["/create-activity", "/manage/:id"].map((path) => (
-            <Route key="activity-form" path={path} element={<ActivityForm />} />
+            <Route path={path} element={<ActivityForm key={location.key} />} />
           ))}
         </Routes>
       </Container>
