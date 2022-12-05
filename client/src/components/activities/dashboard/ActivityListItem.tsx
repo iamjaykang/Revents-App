0e563fd3a959228727fdc5e8dd5d5b7b434fb0fa
Jay Kang <jaykang0395@gmail.com>
2022-12-05 15:51:42 +1300

    feat: added activity filter component

--- client/src/components/activities/dashboard/ActivityListItem.tsx
+++ client/src/components/activities/dashboard/ActivityListItem.tsx
@@ -1,9 +1,8 @@
 import React, { SyntheticEvent, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Icon, Item, Label, Segment } from "semantic-ui-react";
+import { Button, Icon, Item, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
-import userImg from "../../../assets/images/user.png";
 
 interface Props {
   activity: Activity;
