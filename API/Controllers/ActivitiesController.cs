f49052a09ea36d70579134f343920bb5371b56f5
Jay Kang <jaykang0395@gmail.com>
2022-12-05 17:44:59 +1300

    feat: added result handler for CreateActivity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -22,7 +22,7 @@ namespace API.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateActivity(Activity activity)
         {
-            return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
+            return HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
         }
 
         [HttpPut("{id}")]
