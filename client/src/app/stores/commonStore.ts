b52ef2883d5217ccf3ffd09671fb643e9e8a573c
Jay Kang <jaykang0395@gmail.com>
2022-12-08 23:22:31 +1300

    feat: navigate to activities once logged in

--- client/src/app/stores/commonStore.ts
+++ client/src/app/stores/commonStore.ts
@@ -2,13 +2,24 @@ import { makeAutoObservable } from "mobx";
 import { ServerError } from "../models/serverError";
 
 export default class CommonStore {
-    error: ServerError | null = null;
+  error: ServerError | null = null;
+  token: string | null = null;
+  appLoaded = false;
 
-    constructor() {
-        makeAutoObservable(this);
-    }
+  constructor() {
+    makeAutoObservable(this);
+  }
 
-    setServerError = (error: ServerError) => {
-        this.error = error;
-    }
+  setServerError = (error: ServerError) => {
+    this.error = error;
+  };
+
+  setToken = (token: string | null) => {
+    if (token) localStorage.setItem("jwt", token);
+    this.token = token;
+  };
+
+  setAppLoaded = () => {
+    this.appLoaded = true;
+  }
 }
