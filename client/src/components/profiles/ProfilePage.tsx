4212d36ee738e0dcce972116824f9f5ca2c5793c
Jay Kang <jaykang0395@gmail.com>
2022-12-14 20:16:22 +1300

    feat: added displaying photos of user

--- client/src/components/profiles/ProfilePage.tsx
+++ client/src/components/profiles/ProfilePage.tsx
@@ -23,8 +23,12 @@ const ProfilePage = () => {
   return (
     <Grid>
       <Grid.Column width={16}>
-        {profile && <ProfileHeader profile={profile} />}
-        <ProfileContent />
+        {profile && (
+          <>
+            <ProfileHeader profile={profile} />
+            <ProfileContent profile={profile} />
+          </>
+        )}
       </Grid.Column>
     </Grid>
   );
