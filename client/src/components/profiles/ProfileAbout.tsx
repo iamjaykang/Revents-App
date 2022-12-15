cc86bb936678a3feb2e3851eb7d15f68cd9bac62
Jay Kang <jaykang0395@gmail.com>
2022-12-15 20:23:23 +1300

    feat: added bio to profile card and style changes for about me pane

--- client/src/components/profiles/ProfileAbout.tsx
+++ client/src/components/profiles/ProfileAbout.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Grid, Header, Tab } from "semantic-ui-react";
+import { Button, Grid, Header, Item, Tab } from "semantic-ui-react";
 import { Profile } from "../../app/models/profile";
 import { useStore } from "../../app/stores/store";
 import ProfileEditForm from "./ProfileEditForm";
@@ -19,23 +19,43 @@ const ProfileAbout = ({ profile }: Props) => {
       <Grid>
         <Grid.Column width={16}>
           <Header floated="left" icon="address card" content="About" />
-          {isCurrentUser && <Button
-            basic
-            floated="right"
-            content={editMode ? "Cancel" : "Edit Profile"}
-            onClick={() => setEditMode(!editMode)}
-          />}
+          {isCurrentUser && (
+            <Button
+              basic
+              floated="right"
+              content={editMode ? "Cancel" : "Edit Profile"}
+              onClick={() => setEditMode(!editMode)}
+            />
+          )}
         </Grid.Column>
         <Grid.Column width={16}>
           {editMode ? (
             <ProfileEditForm profile={profile} setEditMode={setEditMode} />
           ) : (
-            <>
-              <Header as="h2" sub color="teal" content="Display Name" />
-              <p>{profile.displayName}</p>
-              <Header as="h2" sub color="teal" content="Bio" />
-              <p>{profile.bio}</p>
-            </>
+            <Item.Group>
+              <Item>
+                <Item.Content>
+                  <Item.Header
+                    as="h2"
+                    content="Display Name"
+                    style={{ color: "#00b5ad" }}
+                  />
+                  <Item.Description>{profile.displayName}</Item.Description>
+                </Item.Content>
+              </Item>
+              <Item>
+                <Item.Content>
+                  <Item.Header
+                    as="h2"
+                    content="Bio"
+                    style={{ color: "#00b5ad" }}
+                  />
+                  <Item.Description style={{ whiteSpace: "pre-wrap" }}>
+                    {profile.bio}
+                  </Item.Description>
+                </Item.Content>
+              </Item>
+            </Item.Group>
           )}
         </Grid.Column>
       </Grid>
--- client/src/components/profiles/ProfileCard.tsx
+++ client/src/components/profiles/ProfileCard.tsx
@@ -1,27 +1,33 @@
-import { observer } from 'mobx-react-lite'
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { Card, Icon, Image } from 'semantic-ui-react';
-import { Profile } from '../../app/models/profile'
+import { observer } from "mobx-react-lite";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card, Icon, Image } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
 
 interface Props {
-    profile: Profile;
+  profile: Profile;
 }
 
-const ProfileCard = ({profile}: Props) => {
+const ProfileCard = ({ profile }: Props) => {
   return (
     <Card as={Link} to={`/profiles/${profile.username}`} fluid>
-        <Image src={profile.image || require("../../assets/images/user.png")} />
-        <Card.Content>
-            <Card.Header>{profile.displayName}</Card.Header>
-            <Card.Description>Bio goes here</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-            <Icon name='user'/>
-                20 Followers
-        </Card.Content>
+      <Image src={profile.image || require("../../assets/images/user.png")} />
+      <Card.Content>
+        <Card.Header>{profile.displayName}</Card.Header>
+        <Card.Description
+          style={{ overflow: "hidden", textOverflow: "ellipsis" }}
+        >
+          {profile.bio && profile.bio?.length > 28
+            ? profile.bio.slice(0, 28) + "..."
+            : profile.bio}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Icon name="user" />
+        20 Followers
+      </Card.Content>
     </Card>
-  )
-}
+  );
+};
 
-export default observer(ProfileCard)
+export default observer(ProfileCard);
