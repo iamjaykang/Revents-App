8a5dcddaf74f680fb0ed3f6e3d395b0c61030a4f
Jay Kang <jaykang0395@gmail.com>
2022-12-15 17:19:59 +1300

    feat: added edit controller

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -12,5 +12,10 @@ namespace API.Controllers
             return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
         }
 
+        [HttpPut]
+        public async Task<IActionResult> EditProfile(Edit.Command command)
+        {
+            return HandleResult(await Mediator.Send(command));
+        }
     }
 }
