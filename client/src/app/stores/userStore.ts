db432f667d2b9e6233181112fb05b91b2f5b66f4
Jay Kang <jaykang0395@gmail.com>
2022-12-15 18:04:52 +1300

    feat: added update request and helper method to set displayname in client

--- client/src/app/stores/userStore.ts
+++ client/src/app/stores/userStore.ts
@@ -19,8 +19,8 @@ export default class UserStore {
     try {
       const user = await agent.Account.login(creds);
       store.commonStore.setToken(user.token);
-      runInAction(() => this.user = user);
-      router.navigate('/activities');
+      runInAction(() => (this.user = user));
+      router.navigate("/activities");
       store.modalStore.closeModal();
     } catch (error) {
       throw error;
@@ -31,8 +31,8 @@ export default class UserStore {
     try {
       const user = await agent.Account.register(creds);
       store.commonStore.setToken(user.token);
-      runInAction(() => this.user = user);
-      router.navigate('/activities');
+      runInAction(() => (this.user = user));
+      router.navigate("/activities");
       store.modalStore.closeModal();
     } catch (error) {
       throw error;
@@ -42,19 +42,23 @@ export default class UserStore {
   logout = () => {
     store.commonStore.setToken(null);
     this.user = null;
-    router.navigate('/');
-  }
+    router.navigate("/");
+  };
 
   getUser = async () => {
     try {
       const user = await agent.Account.current();
-      runInAction(() => this.user = user);
+      runInAction(() => (this.user = user));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   setImage = (image: string) => {
     if (this.user) this.user.image = image;
-  }
+  };
+
+  setDisplayName = (name: string) => {
+    if (this.user) this.user.displayName = name;
+  };
 }
